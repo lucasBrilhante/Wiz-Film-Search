83008af6a5fa499aced0ccd9028c80297db5707f
Lucas Brilhante <lbrilhante82@gmail.com>
2020-02-21 21:06:37 -0300

    Create .net core api project

---
+++ Wiz-Film-Search/Wiz-Film-Search/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wiz_Film_Search.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
