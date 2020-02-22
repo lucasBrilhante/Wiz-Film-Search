86075b2bb96f536a416e6acbeef3339e8ce246d0
Lucas Brilhante <lbrilhante82@gmail.com>
2020-02-22 12:24:13 -0300

    Modify models to fit the json structure

--- Wiz-Film-Search/Wiz-Film-Search/Models/Movie.cs
+++ Wiz-Film-Search/Wiz-Film-Search/Models/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +8,19 @@ namespace Wiz_Film_Search.Models
 {
     public class Movie
     {
-        
+        [JsonProperty("genre_ids")]
+        public long[] GenreIds { get; set; }
+
+        [JsonProperty("title")]
         public string Title { get; set; }
-        public string Genrer { get; set; }
+
+        [JsonProperty("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("release_date")]
         public string ReleaseDate { get; set; }
     }
 }
---
+++ Wiz-Film-Search/Wiz-Film-Search/Models/MovieList.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wiz_Film_Search.Models
+{
+    public class MovieList
+    {
+        [JsonProperty("page")]
+        public long Page { get; set; }
+
+        [JsonProperty("total_results")]
+        public long TotalResults { get; set; }
+
+        [JsonProperty("total_pages")]
+        public long TotalPages { get; set; }
+
+        [JsonProperty("results")]
+        public Movie[] Results { get; set; }
+    }
+}
